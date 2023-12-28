64636e9658fc1d5bf1a60f2b354cb3fca6a0881f
Mustafa Gueclue <gueclue.mstf@gmail.com>
2023-12-28 19:54:47 +0100

    feat: add base config class for tests

---
+++ ResilientApi.Test/BaseTests.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using ResilientApi.Data;
+using Xunit.Abstractions;
+
+namespace ResilientApi.Test;
+
+public class BaseTests(ITestOutputHelper output)
+{
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new() { WriteIndented = true };
+
+    protected static DataContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase("CarsDb")
+            .Options;
+
+        return new DataContext(options);
+    }
+    
+    protected void PrintJson(object obj)
+    {
+        output.WriteLine(JsonSerializer.Serialize(obj, JsonSerializerOptions));
+    }
+}
